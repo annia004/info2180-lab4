76de1223531efe855a36abb5100b87913f9586a9
annia <bowesna278@gmail.com>
2022-11-07 08:19:47 -0500

    Edited code to sanitise text field input and make request to superheroes.php

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 window.onload = function() {
     var searchbtn = document.getElementById("search-btn");
-    var url = "http://localhost/info2180-lab4/superheroes.php";
+    var searchbar = document.getElementById("search-bar");
+    var result = document.getElementById("result");
+    
+    var url = "superheroes.php?q=";
 
-    searchbtn.onclick = function() {
+    searchbtn.onclick = function() {        
         var request = new XMLHttpRequest();
+        var searchval = sanitiseString(searchbar.value);
+
+        searchbar.value = "";
 
         request.onreadystatechange = function() {
             if(this.readyState == 4 && this.status == 200) {
-                alert(request.responseText);
+                // alert(request.responseText);
                 // console.log(request.responseText);
+                
+                result.innerHTML = request.responseText;
+
             }
         }; 
         
-        request.open("GET", url, true);
+        request.open("GET", url + searchval, true);
         request.send();       
-    } 
+    }            
+};
+
+// front-end sanitising
+function sanitiseString(str) {
+    if(str.length == 0) {
+        return str;
+    }
+
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "");
+    return str.trim();
 }
-    
